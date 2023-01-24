ff589bbf1b9d65092399ac1f38b4f3d473bf47e4
kaushaldeokar <kaushal.deokar@gmail.com>
2023-01-24 11:50:27 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <main>
-        <header>
-          <a href="/" className="">My Blog</a>
-            <nav>
-              <a href="/">Login </a>
-              <a href="/">Register</a>
-            </nav>
-        </header>
+      <header>
+        <a href="/" className="">
+          My Blog
+        </a>
+        <nav>
+          <a href="/">Login </a>
+          <a href="/">Register</a>
+        </nav>
+      </header>
 
-        <div className="entries">
-          <div className="image">
-            <img src="https://cdn.britannica.com/51/182851-050-97EA5117/Publicity-image-Iron-Man-War-Machine-2.jpg" alt=""  />
-          </div>
-          <div className="text">
+      <div className="entries">
+        <div className="image">
+          <img
+            src="https://cdn.britannica.com/51/182851-050-97EA5117/Publicity-image-Iron-Man-War-Machine-2.jpg"
+            alt=""
+          />
+        </div>
+        <div className="text">
           <h2>Title</h2>
           <p>description</p>
-          </div>
-          <div className="image">
-            <img src="https://cdn.britannica.com/51/182851-050-97EA5117/Publicity-image-Iron-Man-War-Machine-2.jpg" alt=""  />
-          </div>
-          <div className="text">
+        </div>
+        <div className="image">
+          <img
+            src="https://cdn.britannica.com/51/182851-050-97EA5117/Publicity-image-Iron-Man-War-Machine-2.jpg"
+            alt=""
+          />
+        </div>
+        <div className="text">
           <h2>Title</h2>
           <p>description</p>
-          </div>
-          
-          
-            
-
-
-          </div>
-        
+        </div>
+      </div>
     </main>
   );
 }
